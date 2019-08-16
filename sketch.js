ce9e6457f4edc5934201e8d7c6e11b08c53ad4d5
Thib03 <41568449+Thib03@users.noreply.github.com>
2019-08-17 00:03:01 +0200

    oué

--- sketch.js
+++ sketch.js
@@ -1,82 +1,160 @@
 var black = 0;
 var white = 255;
-var note;
+
+var dimension;
+var weight = 0.005;
+var bigRadius = 0.35;
+var littleRadius = 0.0905;
+
+var angle;
+
+var notes = [];
 var millisecond = 0;
-var buttonPressed = -1;
+var notePressed = -1;
+
+function degToNdt(d) {
+  switch(d) {
+    default:
+    case 1: return 0;
+    case 2: return 2;
+    case 3:	return 4;
+    case 4: return 5;
+    case 5: return 7;
+    case 6: return 9;
+		case 7: return 11;
+  }
+}
 
 class Note {
-  constructor(t,/*i,*/x,y,l,h) {
-    this.button = new Clickable(width /2+x-l/2,
-                                height/2+y-h/2);
-    this.button.resize(l,h);
+  constructor(degree) {
+    this.d = degree;
+    this.n = degToNdt(degree);
+
+    this.angle = PI/2 - this.n*PI/6;
+
+    this.button = new Clickable();
     this.button.color = 255;
-    this.button.cornerRadius = dimension();
-    this.button.strokeWeight = 0.0034*dimension();
+    this.button.cornerRadius = 1000;
     this.button.stroke = black;
     this.button.text = '';
 
-    this.t = t;
-    this.tc = black;
+    this.updateText();
+    this.textColor = black;
+
+    var deg = this.d;
 
     this.button.onPress = function() {
-      if(millis()-millisecond > 500) {
-        if(buttonPressed == -1) {
-          buttonPressed = 0;
-        }
-        millisecond = millis();
-      }
-
-      this.locate(mouseX-l/2,mouseY-h/2);
+      notePressed = deg-1;
     }
-
-    /*this.button.onHover = function() {
-      cursor(HAND);
-    }*/
 
     this.button.onRelease = function() {
-      buttonPressed = -1;
+      if(notePressed > -1) {
+        var note = notes[notePressed];
+        notePressed = -1;
+        var x = mouseX-width/2;
+        var y = -(mouseY-height/2);
+        var a = Math.atan(y/x);
+        if(x < 0) {
+          a += PI;
+        }
+        var min = (notes[(note.d+5)%7].n+1)%12;
+        var max =  notes[(note.d  )%7].n;
+        for(let n = min; n != max; n = (n+1)%12) {
+          var da = PI/2 - n*PI/6 - a;
+          while(da < 0)     {da += 2*PI;}
+          while(da >= 2*PI) {da -= 2*PI;}
+          if(da >= 2*PI - PI/12 || da < PI/12) {
+            if(n != note.n) {
+              note.n = n;
+            }
+          }
+        }
+        note.angle = PI/2 - note.n*PI/6;
+        note.updateText();
+        note.update();
+      }
     }
 
-    /*this.button.onOutside = function() {
-      cursor(ARROW);
-    }*/
+    this.update();
+  }
+
+  updateText() {
+    var text = '';
+    switch(this.d) {
+      default:
+      case 1: text += 'C'; break;
+      case 2: text += 'D'; break;
+      case 3: text += 'E'; break;
+      case 4: text += 'F'; break;
+      case 5: text += 'G'; break;
+      case 6: text += 'A'; break;
+      case 7: text += 'B'; break;
+    }
+    switch(this.n-degToNdt(this.d)) {
+      case -2: text += 'bb'; break;
+      case -1: text += 'b';  break;
+      case  0:               break;
+      case  1: text += '#';  break;
+      case  2: text += '##'; break;
+      default: text += '?';
+    }
+    this.text = text;
+  }
+
+  update() {
+    this.button.resize(2*littleRadius*dimension,2*littleRadius*dimension);
+    this.button.locate(width/2 +bigRadius*dimension*cos(this.angle)-littleRadius*dimension,
+                       height/2-bigRadius*dimension*sin(this.angle)-littleRadius*dimension);
+    this.button.strokeWeight = weight*dimension;
+  }
+
+  move() {
+    var x = mouseX-width/2;
+    var y = -(mouseY-height/2);
+    let r = sqrt(pow(x,2)+pow(y,2));
+    if(r >= (bigRadius-littleRadius)*dimension &&
+       r <  (bigRadius+littleRadius)*dimension) {
+      var a = Math.atan(y/x);
+      if(x < 0) {
+        a += PI;
+      }
+      this.angle = a;//lerp(this.angle,a,0.8);
+    }
+    else {
+      this.angle = PI/2 - this.n*PI/6;
+      notePressed = -1;
+    }
+    this.update();
+    this.draw();
   }
 
   drawText() {
-    fill(this.tc);
+    fill(this.textColor);
     textAlign(CENTER,CENTER);
-    textSize(0.4*pow(pow(this.button.width,2)*this.button.height,1/3));
-    //textFont(font);
-    text(this.t,this.button.x+this.button.width/2,
-                this.button.y+this.button.height/2+0.003*dimension());
+    textSize(0.475*pow(pow(this.button.width,2)*this.button.height,1/3));
+    textFont(font);
+    text(this.text,this.button.x+this.button.width/2,
+                   this.button.y+this.button.height/2-0.009*dimension);
   }
 
   draw() {
     this.button.draw();
     this.drawText();
-    //image(this.i,0,0,50,200);
-  }
-
-  toggle(bool) {
-    if(bool) {
-      this.button.color = black;
-      this.tc = white;
-    }
-    else {
-      this.button.color = white;
-      this.tc = black;
-    }
   }
 }
 
-function dimension() {
-  return sqrt(width*height);
+function preload() {
+  font = loadFont('nunito.ttf');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  note = new Note('X',0,0,0.1*dimension(),0.1*dimension());
+  dimension = Math.min(width,height);
+
+  for(let d = 1; d <= 7; d++) {
+    notes.push(new Note(d));
+  }
 }
 
 function draw() {
@@ -84,13 +162,34 @@ function draw() {
 
   noFill();
   stroke(black);
-  strokeWeight(0.0034*dimension());
-  circle(width/2,height/2,0.75*dimension(),0.75*dimension());
+  strokeWeight(weight*dimension);
+  circle(width/2,height/2,2*bigRadius*dimension,2*bigRadius*dimension);
 
-  note.draw();
+  for(let n = 0; n < 12; n++) {
+    let a = PI/2 - n*PI/6;
+    let r = bigRadius*dimension;
+    let dr = 0.65*littleRadius*dimension;
+    line(width/2+(r+dr)*cos(a),height/2-(r+dr)*sin(a),
+         width/2+(r-dr)*cos(a),height/2-(r-dr)*sin(a));
+  }
 
-  if(buttonPressed > -1) {
-    note.button.locate(mouseX-note.button.width/2,
-                       mouseY-note.button.height/2);
+  for(let n = 0; n < notes.length; n++) {
+    if(n != notePressed) {
+      notes[n].draw();
+    }
+  }
+
+  if(notePressed > -1) {
+    notes[notePressed].move();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+  dimension = Math.min(width,height);
+
+  for(let n = 0; n < notes.length; n++) {
+    notes[n].update();
   }
 }
