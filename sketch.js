b130dd9b4b7b4977ddb1192aac242d485ff10240
Thib03 <41568449+Thib03@users.noreply.github.com>
2019-08-16 14:10:15 +0200

    first

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var black = 0;
+var white = 255;
+var button;
+
+class Button {
+  constructor(t,/*i,*/x,y,l,h) {
+    this.button = new Clickable(width /2+x-l/2,
+                                height/2+y-h/2);
+    this.button.resize(l,h);
+    this.button.color = 255;
+    this.button.cornerRadius = dimension();
+    this.button.strokeWeight = 0.0034*dimension();
+    this.button.stroke = black;
+    this.button.text = '';
+
+    this.t = t;
+    this.tc = black;
+    //this.i = i;
+    this.x = x;
+    this.y = y;
+    this.l = l;
+    this.h = h;
+
+    this.button.onOutside = function() {
+      cursor(ARROW);
+    }
+
+    this.button.onHover = function(){
+      cursor(HAND);
+    }
+
+    /*this.button.onRelease = function(){
+      this.color = 255;
+    }*/
+  }
+
+  drawText() {
+    fill(this.tc);
+    textAlign(CENTER,CENTER);
+    textSize(0.4*pow(pow(this.l,2)*this.h,1/3));
+    //textFont(font);
+    text(this.t,width/2+this.x,
+         height/2+this.y+0.003*dimension());
+  }
+
+  draw() {
+    this.button.draw();
+    this.drawText();
+    //image(this.i,0,0,50,200);
+  }
+
+  toggle(bool) {
+    if(bool) {
+      this.button.color = black;
+      this.tc = white;
+    }
+    else {
+      this.button.color = white;
+      this.tc = black;
+    }
+  }
+}
+
+function dimension() {
+  return sqrt(width*height);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  button = new Button('A',0,0,0.1*dimension(),0.1*dimension());
+  button.button.onPress = function() {
+      if(button.button.color == 255) {
+        button.toggle(true);
+      }
+      else {
+        button.toggle(false);
+      }
+  }
+}
+
+function draw() {
+  background(255);
+
+  button.draw();
+}
